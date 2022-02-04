aa15c5224d64f74226f99e1d911e147b429f6da3
Craig Lockwood <craiglockwood@me.com>
2022-02-04 23:31:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,12 @@ function App() {
 
   return (
      <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div className="w-full top-0 py-3 px-3 text-center text-white bg-violet-900">Brought to you by the Knife Talk podcast</div>
+      <div className="w-full top-0 py-3 px-3 text-center text-white bg-violet-900">Brought to you by the Knife Talk podcast  <a
+          href="https://github.com/hannahcode/GAME"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a></div>
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl grow font-bold dark:text-white">{GAME_TITLE}</h1>
         <SunIcon
