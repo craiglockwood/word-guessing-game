7fd8f75a5bc83d9c181f68745212e643bcc477d0
Craig Lockwood <craiglockwood@me.com>
2022-02-04 19:28:46 +0100

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,7 +9,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is a knife themed clone of Wordle based around the open source version created by Hannah Park -{' '}
+        This is a knife themed clone of Wordle brought to you buy the folks at the Knife Talk podcast. The code is based around the open source version created by Hannah Park -{' '}
         <a
           href="https://github.com/hannahcode/GAME"
           className="underline font-bold"
