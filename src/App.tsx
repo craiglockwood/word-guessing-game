e853e1d74da48e56aa2adaf1efb6dc64c5b650c2
Craig Lockwood <craiglockwood@me.com>
2022-02-05 00:05:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -162,7 +162,7 @@ function App() {
         >
           Spotify
         </a> 
-    </div> 
+    
     <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl grow font-bold dark:text-white">{GAME_TITLE}</h1>
